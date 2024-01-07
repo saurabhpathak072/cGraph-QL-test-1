49e1cf77a69f2e06e206f5ebf885781041307c58
Saurabh Pathak <SPathak@eagle.org>
2024-01-08 00:42:26 +0530

    added Client

---
+++ src/GraphQL/Mutation.js
@@ -0,0 +1,6 @@
+import { useMutation } from '@apollo/client';
+import { CREATE_NEW_STUDENT } from './Query';
+
+// export const createNewStudentGraphQL =()=>{
+//     const [] =useMutation(CREATE_NEW_STUDENT);
+// }
